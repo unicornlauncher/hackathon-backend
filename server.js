ac8fdff6a6f02cefe8110d2b49be08d91eba2586
kaio <silveira.kaio@icloud.com>
2020-09-18 23:36:55 +0100

    Add room code generator

--- server.js
+++ server.js
@@ -20,10 +20,35 @@ app.use(`${BASE_URL}/v1/ping`, (req, res) => res.send('pong'));
 
 const roomRouter = express.Router();
 
+class RoomCodeGenerator {
+  static generate() {
+    const alphabet = 'abcdefghijklmnopqrstuvxwyz';
+    const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0];
+
+    let code = '';
+    for (let i = 0; i <= 4; i++) {
+      code +=
+        parseInt(Math.random() * 2) % 2 === 0
+          ? alphabet[
+              parseInt(Math.random() * 99999) % alphabet.length
+            ].toUpperCase()
+          : numbers[parseInt(Math.random() * 99999) % numbers.length];
+    }
+
+    return code;
+  }
+}
+
 roomRouter.route('/').post((req, res) => {
   const { roomName, userName } = req.body;
   const owner = { _id: uuid(), userName };
-  const room = { _id: uuid(), roomName, owner, participants: [owner] };
+  const room = {
+    _id: uuid(),
+    roomName,
+    owner,
+    code: RoomCodeGenerator.generate(),
+    participants: [owner],
+  };
   memory.set(room._id, JSON.stringify(room), err => {
     if (err) {
       return res.status(500).json({ msg: 'Ooops' });
@@ -33,7 +58,6 @@ roomRouter.route('/').post((req, res) => {
 });
 
 roomRouter.route('/:id').get((req, res) => {
-  console.log(`id: ${req.params.id}`);
   memory.get(req.params.id, (err, room) => {
     if (err) {
       return res.status(500).json({ msg: 'Oops' });
