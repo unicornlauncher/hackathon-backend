50a8b0f1a76c3a6c847aa892bba74c54e98a5d86
kaio <silveira.kaio@icloud.com>
2020-09-18 23:27:07 +0100

    Add endpoints to create and retrieve room info

--- server.js
+++ server.js
@@ -5,13 +5,45 @@ const express = require('express');
 const http = require('http');
 const morgan = require('morgan');
 const cors = require('cors');
+const { uuid } = require('uuidv4');
+const redis = require('redis');
+const bodyParser = require('body-parser');
+const memory = redis.createClient();
 
 const app = express();
 
 app.use(cors());
+app.use(bodyParser.json({}));
 app.use(morgan('dev'));
 
 app.use(`${BASE_URL}/v1/ping`, (req, res) => res.send('pong'));
+
+const roomRouter = express.Router();
+
+roomRouter.route('/').post((req, res) => {
+  const { roomName, userName } = req.body;
+  const owner = { _id: uuid(), userName };
+  const room = { _id: uuid(), roomName, owner, participants: [owner] };
+  console.log(room, JSON.stringify(room));
+  memory.set(room._id, JSON.stringify(room), err => {
+    if (err) {
+      return res.status(500).json({ msg: 'Ooops' });
+    }
+    return res.status(201).json(room);
+  });
+});
+
+roomRouter.route('/:id').get((req, res) => {
+  console.log(`id: ${req.params.id}`);
+  memory.get(req.params.id, (err, room) => {
+    if (err) {
+      return res.status(500).json({ msg: 'Oops' });
+    }
+    return res.json(JSON.parse(room));
+  });
+});
+
+app.use(`${BASE_URL}/v1/rooms`, roomRouter);
 
 http.createServer(app).listen(PORT, () => {
   console.log(`> 🚀 Server running at ${PORT}`);
