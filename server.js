a4c650abcdf0202fe1e82a8dac41e49d4acc29aa
kaio <silveira.kaio@icloud.com>
2020-09-19 23:24:48 +0100

    Encapsulate access to memory database

--- server.js
+++ server.js
@@ -8,10 +8,12 @@ const cors = require('cors');
 const { uuid } = require('uuidv4');
 const redis = require('redis');
 const bodyParser = require('body-parser');
-const memory = redis.createClient();
+// const memory = redis.createClient();
 const IOServer = require('socket.io');
 const app = express();
 const io = new IOServer();
+
+app.io = io;
 
 app.use(cors());
 app.use(bodyParser.json({}));
@@ -20,6 +22,50 @@ app.use(morgan('dev'));
 app.use(`${BASE_URL}/v1/ping`, (req, res) => res.send('pong'));
 
 const roomRouter = express.Router();
+
+class MemoryDatabase {
+  constructor({ redisClient }) {
+    this.memory = redisClient;
+  }
+
+  async keys(pattern) {
+    return new Promise((resolve, reject) => {
+      this.memory.keys(pattern, (err, keys) => {
+        if (err) {
+          reject(err);
+        }
+
+        resolve(keys);
+      });
+    });
+  }
+  async set(key, value) {
+    console.log(value);
+    return new Promise((resolve, reject) => {
+      this.memory.set(key, JSON.stringify(value), err => {
+        if (err) {
+          reject(err);
+        }
+
+        resolve();
+      });
+    });
+  }
+
+  async get(key) {
+    return new Promise((resolve, reject) => {
+      this.memory.get(key, (err, value) => {
+        if (err) {
+          reject(err);
+        }
+
+        resolve(JSON.parse(value));
+      });
+    });
+  }
+}
+
+const memory = new MemoryDatabase({ redisClient: redis.createClient() });
 
 class RoomCodeGenerator {
   static generate() {
@@ -40,78 +86,102 @@ class RoomCodeGenerator {
   }
 }
 
-const INTERNAL_ERROR = res => res.status(500).json({ msg: 'Oops' });
+const internalServerError = res => res.status(500).json({ msg: 'Oops' });
 
-roomRouter.route('/').post((req, res) => {
-  const { roomName, userName } = req.body;
-  const owner = { _id: uuid(), userName };
-  const room = {
-    _id: uuid(),
-    roomName,
-    owner,
-    code: RoomCodeGenerator.generate(),
-    participants: [owner],
-  };
-  memory.set(room._id, JSON.stringify(room), err => {
-    if (err) {
-      return res.status(500).json({ msg: 'Ooops' });
-    }
+roomRouter.route('/').post(async (req, res) => {
+  try {
+    const { roomName, userName } = req.body;
+    const owner = { _id: uuid(), userName };
+    const room = {
+      _id: uuid(),
+      roomName,
+      owner,
+      code: RoomCodeGenerator.generate(),
+      participants: [owner],
+    };
+
+    await memory.set(room._id, room);
     return res.status(201).json(room);
-  });
+  } catch (ex) {
+    console.log(ex);
+    return internalServerError(res);
+  }
 });
 
-roomRouter.route('/:id').get((req, res) => {
-  memory.get(req.params.id, (err, room) => {
-    if (err) {
-      return INTERNAL_ERROR(res);
-    }
-    return res.json(JSON.parse(room));
-  });
+roomRouter.route('/:id').get(async (req, res) => {
+  try {
+    const room = await memory.get(req.params.id);
+    return res.json(room);
+  } catch (ex) {
+    console.log(ex);
+    return internalServerError(res);
+  }
 });
 
-roomRouter.route('/:id/config').post((req, res) => {
-  const { sequence } = req.body;
-  memory.get(req.params.id, (err, roomStr) => {
-    if (err) {
-      return res.status(500).json({ msg: 'Ooops' });
-    }
-
-    const room = JSON.parse(roomStr);
+roomRouter.route('/:id/config').post(async (req, res) => {
+  try {
+    const { sequence } = req.body;
+    const room = await memory.get(req.params.id);
     console.log(room._id, { ...room, sequence });
     const updated = { ...room, sequence };
-    memory.set(room._id, JSON.stringify(updated), err => {
-      if (err) {
-        console.log(err);
-        return res.status(500).json({ msg: 'Ooops' });
-      }
-
-      return res.json(updated);
-    });
-  });
+    await memory.set(room._id, updated);
+    return res.json(updated);
+  } catch (ex) {
+    console.log(ex);
+    return internalServerError(res);
+  }
 });
 
-roomRouter.route('/:id/cards').post((req, res) => {
-  const { cards } = req.body;
-  memory.get(req.params.id, (err, roomStr) => {
-    const room = JSON.parse(roomStr);
-    if (err) {
-      console.log(err);
-      return INTERNAL_ERROR(res);
-    }
-
+roomRouter.route('/:id/cards').post(async (req, res) => {
+  try {
+    const { cards } = req.body;
+    const room = await memory.get(req.params.id);
     const updated = {
       ...room,
       cards: [...(room.cards || []), cards.map(c => ({ ...c, _id: uuid() }))],
     };
-    memory.set(room._id, JSON.stringify(updated), err => {
-      if (err) {
-        console.log(err);
-        return INTERNAL_ERROR(res);
-      }
 
-      return res.status(201).json(updated);
+    await memory.set(room._id, updated);
+    return res.status(201).json(updated);
+  } catch (err) {
+    console.log(err);
+    return internalServerError(res);
+  }
+});
+
+roomRouter.route('/join/:roomCode').post(async (req, res) => {
+  try {
+    const { roomCode } = req.params;
+    const { name } = req.body;
+
+    if (!roomCode) {
+      return res.status(400).json({ msg: 'Expected a room code' });
+    }
+
+    const keys = await memory.keys('*');
+
+    const rooms = (
+      await Promise.all(
+        keys.map(async key => ({ key, value: await memory.get(key) }))
+      )
+    ).map(({ value }) => value);
+
+    const room = rooms.find(({ code }) => code === roomCode);
+
+    if (!room) {
+      return res.status(400).json({ msg: 'Room not found' });
+    }
+
+    await memory.set({
+      ...room,
+      participants: [...room.participants, { name, _id: uuid() }],
     });
-  });
+
+    return res.json(room);
+  } catch (ex) {
+    console.log(ex);
+    return internalServerError(res);
+  }
 });
 
 app.use(`${BASE_URL}/v1/rooms`, roomRouter);
